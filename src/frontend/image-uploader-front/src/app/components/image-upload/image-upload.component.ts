d3d2c507237810dc000e245b1bfcf6a100b09b87
ElliotLuque <elliotluque@gmail.com>
2022-08-29 01:05:53 +0200

    Refactored image upload component

--- src/frontend/image-uploader-front/src/app/components/image-upload/image-upload.component.ts
+++ src/frontend/image-uploader-front/src/app/components/image-upload/image-upload.component.ts
@@ -1,18 +1,20 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import {ImageUploadService} from "../../image-upload-service";
 
 @Component({
   selector: 'app-image-upload',
   templateUrl: './image-upload.component.html',
   styleUrls: ['./image-upload.component.css', 'image-upload.component-media-queries.css']
 })
-export class ImageUploadComponent implements OnInit {
+export class ImageUploadComponent{
 
-  ngOnInit(): void {
+  constructor(private imageUploadService: ImageUploadService) {
   }
-
 
   onFileInput(event: any) {
-
-
+    this.imageUploadService.selectFile(event);
+    this.imageUploadService.uploadFile();
   }
+
+
 }
