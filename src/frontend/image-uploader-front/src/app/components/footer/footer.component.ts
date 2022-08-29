40af76bd94fb6a5b473d364ce994f1e6211e2b9f
ElliotLuque <elliotluque@gmail.com>
2022-08-29 18:37:19 +0200

    Refactor footer class

--- src/frontend/image-uploader-front/src/app/components/footer/footer.component.ts
+++ src/frontend/image-uploader-front/src/app/components/footer/footer.component.ts
@@ -1,18 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css', 'footer.component-media-queries.css']
 })
-export class FooterComponent implements OnInit {
-
+export class FooterComponent {
   authorRepository: String = 'https://github.com/ElliotLuque';
   devChallenges: String = 'https://devchallenges.io/';
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
 }
