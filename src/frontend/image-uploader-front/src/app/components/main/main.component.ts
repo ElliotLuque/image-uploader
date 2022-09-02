2cb629f6457d7379885a5f69623691705065a31b
ElliotLuque <elliotluque@gmail.com>
2022-09-02 21:19:26 +0200

    Fix main component module

--- src/frontend/image-uploader-front/src/app/components/main/main.component.ts
+++ src/frontend/image-uploader-front/src/app/components/main/main.component.ts
@@ -5,7 +5,6 @@ import {ImageUploadService} from "../../services/image-upload-service";
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
 })
 export class MainComponent  {
 
